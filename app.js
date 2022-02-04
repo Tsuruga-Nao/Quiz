d8a8e35ffccf9d0b8e1016870651d9db71b6a6d7
Tsuruga-Nao <98831750+Tsuruga-Nao@users.noreply.github.com>
2022-02-04 11:56:57 +0900

    ver1.0.3

--- app.js
+++ app.js
@@ -42,59 +42,59 @@
       index++;    
   }
 
-const quiz = [
-  {
-    question: 'ゲーム市場、最も売れたゲーム機は次のうちどれ？',
-    answers: [
-      'スーパーファミコン',
-      'プレイステーション２',
-      'ニンテンドースイッチ',
-      'ニンテンドーDS'
-    ],
-    correct: 'ニンテンドーDS'
-  },  {
-    question: '糸井重里が規格に関わった、任天堂の看板ゲームといえば？',
-    answers: [
-      'MOTHER2',
-      'スーパーマリオブラザーズ３',
-      'スーパードンキーコング',
-      '星のカービィ'
-    ],
-    correct: 'MOTHER2'
-  },  {
-    question: 'ファイナルファンタジーIVの主人公の名前は？',
-    answers: [
-      'フリオニール',
-      'クラウド',
-      'ティーダ',
-      'セシル'
-    ],
-    correct: 'セシル'
-  },
-];
+  const quiz = [
+    {
+      question: 'ゲーム市場、最も売れたゲーム機は次のうちどれ？',
+      answers: [
+        'スーパーファミコン',
+        'プレイステーション２',
+        'ニンテンドースイッチ',
+        'ニンテンドーDS'
+      ],
+      correct: 'ニンテンドーDS'
+    },  {
+      question: '糸井重里が規格に関わった、任天堂の看板ゲームといえば？',
+      answers: [
+        'MOTHER2',
+        'スーパーマリオブラザーズ３',
+        'スーパードンキーコング',
+        '星のカービィ'
+      ],
+      correct: 'MOTHER2'
+    },  {
+      question: 'ファイナルファンタジーIVの主人公の名前は？',
+      answers: [
+        'フリオニール',
+        'クラウド',
+        'ティーダ',
+        'セシル'
+      ],
+      correct: 'セシル'
+    },
+  ];
 
-const quizLength = quiz.length;
-let quizIndex = 0;
-let score = 0;
+  const quizLength = quiz.length;
+  let quizIndex = 0;
+  let score = 0;
 
-const $button = document.getElementsByTagName('button');
-const buttonLength = $button.length;
-const result = document.getElementById('result');
-const container = document.getElementById('container')
+  const $button = document.getElementsByTagName('button');
+  const buttonLength = $button.length;
+  const result = document.getElementById('result');
+  const container = document.getElementById('container')
 
-//クイズの問題文、選択肢を定義
-const setupQuiz = () => {
-  document.getElementById('js-qusetion').textContent = '問題： ' + quiz[quizIndex].question;
-  let buttonIndex = 0;
-  while (buttonIndex < buttonLength){
-    $button[buttonIndex].textContent = quiz[quizIndex].answers[buttonIndex];
-    buttonIndex++;
+  //クイズの問題文、選択肢を定義
+  const setupQuiz = () => {
+    document.getElementById('js-qusetion').textContent = '問題： ' + quiz[quizIndex].question;
+    let buttonIndex = 0;
+    while (buttonIndex < buttonLength){
+      $button[buttonIndex].textContent = quiz[quizIndex].answers[buttonIndex];
+      buttonIndex++;
+    }
   }
-}
-setupQuiz();
+  setupQuiz();
 
 
-const clickHandler = (e) => {
+  const clickHandler = (e) => {
     if(quiz[quizIndex].correct === e.target.textContent){
     window.alert('正解！');
     score++;
@@ -120,17 +120,20 @@ const clickHandler = (e) => {
     }
   
 
-};
+  };
 
 
-let handlerIndex = 0;
-while (handlerIndex < buttonLength) {
-  $button[handlerIndex].addEventListener('click',(e) => {
-    clickHandler(e);
+  let handlerIndex = 0;
+  while (handlerIndex < buttonLength) {
+    $button[handlerIndex].addEventListener('click',(e) => {
+      clickHandler(e);
+    });
+    handlerIndex++;
+  }
+
+  let btnReload = document.getElementById('btnReload');
+  btnReload.addEventListener('click', function(){
+    location.reload();
   });
-  handlerIndex++;
-}
-
-
 
 })();
